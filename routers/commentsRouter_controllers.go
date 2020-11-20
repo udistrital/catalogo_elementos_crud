66c8432ee858a3c0da237bc7eccb41e57ddc6bdc
CristianDavidSanchez <crdsanchezb@correo.udistrital.edu.co>
2020-11-20 14:34:59 -0500

    Se ajusta el api con los controladores, modelos y rutas para la adhesion de la relacion de niveles, el migrations ya se realizo tambien #39

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -403,6 +403,42 @@ func init() {
 			Filters:          nil,
 			Params:           nil})
 
+	beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"] = append(beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"] = append(beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"] = append(beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"] = append(beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:RelacionNivelController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
 	beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:TrCatalogoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/catalogo_elementos_crud/controllers:TrCatalogoController"],
 		beego.ControllerComments{
 			Method:           "GetOne",
