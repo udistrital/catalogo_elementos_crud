74fa1afe56ff7769f9ccd60bad0c9357b895329a
ludwinggsanchez <ludwingsanchezleon@gmail.com>
2021-09-21 17:54:11 -0500

    feat: udistrital/arka_cliente#787: Ajuste en el controlador: ya no se requiere enviar las fechas

--- controllers/subgrupo.go
+++ controllers/subgrupo.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/udistrital/catalogo_elementos_crud/models"
-	"github.com/udistrital/utils_oas/time_bogota"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
@@ -37,8 +36,6 @@ func (c *SubgrupoController) URLMapping() {
 func (c *SubgrupoController) Post() {
 	var v models.Subgrupo
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
-		v.FechaCreacion = time_bogota.TiempoBogotaFormato()
-		v.FechaModificacion = time_bogota.TiempoBogotaFormato()
 		if _, err := models.AddSubgrupo(&v); err == nil {
 			c.Ctx.Output.SetStatus(201)
 			c.Data["json"] = v
@@ -161,8 +158,6 @@ func (c *SubgrupoController) Put() {
 	id, _ := strconv.Atoi(idStr)
 	v := models.Subgrupo{Id: id}
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
-		v.FechaCreacion = time_bogota.TiempoCorreccionFormato(v.FechaCreacion)
-		v.FechaModificacion = time_bogota.TiempoBogotaFormato()
 		if err := models.UpdateSubgrupoById(&v); err == nil {
 			c.Data["json"] = v
 		} else {
