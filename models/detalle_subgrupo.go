95c70ad546874ac0f343f72f46bec3d52662fa25
Alex F. Bustos P <alexfbp@gmail.com>
2022-08-08 14:30:06 -0500

    feat: soporte a query `__in`

--- models/detalle_subgrupo.go
+++ models/detalle_subgrupo.go
@@ -63,7 +63,7 @@ func GetAllDetalleSubgrupo(query map[string]string, fields []string, sortby []st
 		k = strings.Replace(k, ".", "__", -1)
 		if strings.Contains(k, "isnull") {
 			qs = qs.Filter(k, (v == "true" || v == "1"))
-		} else if strings.Contains(k, "in") {
+		} else if strings.HasSuffix(k, "__in") {
 			arr := strings.Split(v, "|")
 			qs = qs.Filter(k, arr)
 		} else {
