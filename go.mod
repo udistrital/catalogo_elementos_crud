d204e84b8b812635db03eb1e531aa2316cdb5831
lud <luasanchezl@udistrital.edu.co>
2026-01-07 09:00:26 -0500

    feat: actualización aws sdk
    - Consulta de credenciales con aws sdk v2

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.25
 
 require (
 	github.com/astaxie/beego v1.12.3
+	github.com/aws/aws-sdk-go-v2 v1.41.0
+	github.com/aws/aws-sdk-go-v2/config v1.32.6
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.67.7
 	github.com/lib/pq v1.10.9
 	github.com/udistrital/utils_oas v0.1.1-beta.1
 )
@@ -11,7 +14,19 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/aws/aws-sdk-go v1.44.316 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.19.6 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.16 // indirect
+	github.com/aws/aws-sdk-go-v2/service/signin v1.0.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.30.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.41.5 // indirect
 	github.com/aws/aws-xray-sdk-go v1.8.1 // indirect
+	github.com/aws/smithy-go v1.24.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
 	"github.com/astaxie/beego/plugins/cors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	_ "github.com/lib/pq"
 
 	_ "github.com/udistrital/catalogo_elementos_crud/routers"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/udistrital/utils_oas/auditoria"
 	"github.com/udistrital/utils_oas/customerrorv2"
-	"github.com/udistrital/utils_oas/database"
 	"github.com/udistrital/utils_oas/security"
 	"github.com/udistrital/utils_oas/xray"
 )
 
 func main() {
-	conn, err := database.BuildPostgresConnectionString()
+	conn, err := buildPostgresConnectionString()
 	if err != nil {
 		logs.Error("error consultando la cadena de conexión: %v", err)
 		return
@@ -59,4 +66,62 @@ func main() {
 	beego.ErrorController(&customerrorv2.CustomErrorController{})
 	security.SetSecurityHeaders()
 	beego.Run()
+}
+
+func buildPostgresConnectionString() (string, error) {
+	parameterPrefix := beego.AppConfig.String("parameterStore")
+	if parameterPrefix == "" {
+		logs.Info("usando credenciales locales para la conexión a la base de datos")
+		return formatPostgresConnectionString(beego.AppConfig.String("PGuser"), beego.AppConfig.String("PGpass")), nil
+	}
+
+	parameterBasePath := beego.AppConfig.String("appname") + "/db/"
+
+	username, err := getParameterFromParameterStore(context.Background(), parameterBasePath+"username")
+	if err != nil {
+		logs.Critical("error consultando username: %v", err)
+		return "", errors.New("error consultando credenciales: " + err.Error())
+	}
+
+	password, err := getParameterFromParameterStore(context.Background(), parameterBasePath+"password")
+	if err != nil {
+		logs.Critical("error consultando credenciales: %v", err)
+		return "", errors.New("error consultando credenciales: " + err.Error())
+	}
+
+	conn := formatPostgresConnectionString(username, password)
+
+	return conn, nil
+}
+
+func getParameterFromParameterStore(ctx context.Context, name string) (string, error) {
+	parameterName := addPrefix(name)
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return "", fmt.Errorf("unable to load SDK config: %w", err)
+	}
+
+	ssmClient := ssm.NewFromConfig(cfg)
+	output, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
+		Name:           aws.String(parameterName),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		return "", fmt.Errorf("ocurrió un error al consultar el parámetro: %w", err)
+	}
+
+	return *output.Parameter.Value, nil
+}
+
+func addPrefix(path string) string {
+	parameterPrefix := beego.AppConfig.String("parameterStore")
+	if parameterPrefix != "" {
+		return "/" + parameterPrefix + "/" + path
+	}
+	return path
+}
+
+func formatPostgresConnectionString(username, password string) string {
+	return "postgres://" + username + ":" + url.QueryEscape(password) + "@" + beego.AppConfig.String("PGhost") + ":" + beego.AppConfig.String("PGport") + "/" + beego.AppConfig.String("PGdb") + "?sslmode=disable&search_path=" + beego.AppConfig.String("PGschema")
 }
